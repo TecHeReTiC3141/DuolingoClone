ba6828b6c22bb2004d3e76fcd9f9fd86a5ffaba5
TecHeReTiC3141 <talex73@mail.ru>
2024-04-05 17:09:44 +0300

    Clerk has been installed and set up

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,25 @@
 import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
-const font = Nunito({ subsets: ["latin"] });
+const font = Nunito({ subsets: [ "latin" ] });
 
 export const metadata: Metadata = {
-  title: "Duolingo Clone",
-  description: "Clone of duolingo",
+    title: "Duolingo Clone",
+    description: "Clone of duolingo",
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={font.className}>{children}</body>
-    </html>
-  );
+    return (
+        <ClerkProvider>
+            <html lang="en">
+            <body className={font.className}>{children}</body>
+            </html>
+        </ClerkProvider>
+    );
 }
