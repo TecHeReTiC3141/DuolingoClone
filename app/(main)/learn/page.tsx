b605928081552e5b4cb2d34d4a62f191b86d2899
TecHeReTiC3141 <talex73@mail.ru>
2024-04-07 14:49:50 +0300

    DB setup is in progress. LearnHeader and UserProgress components have been created

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,24 +1,28 @@
 import { StickyWrapper } from "@/components/StickyWrapper";
 import { FeedWrapper } from "@/components/FeedWrapper";
+import { LearnHeader } from "@/app/(main)/learn/LearnHeader";
+import { UserProgress } from "@/components/UserProgress";
 
 export default function LearnPage() {
     return (
         <div className="flex flex-row-reverse gap-[48px] px-6">
             <StickyWrapper>
-                Sticky wrapper
+                <UserProgress
+                    activeCourse={{title: "Spanish", imageSrc: "/flags/es.svg"}}
+                    hearts={5}
+                    points={100}
+                    hasActiveSubscription={true}
+                />
             </StickyWrapper>
             <FeedWrapper>
-                <div className="bg-blue-500 h-56 w-full" />
-                <div className="bg-blue-500 h-56 w-full" />
-                <div className="bg-blue-500 h-56 w-full" />
-                <div className="bg-blue-500 h-56 w-full" />
-                <div className="bg-blue-500 h-56 w-full" />
-                <div className="bg-blue-500 h-56 w-full" />
-                <div className="bg-blue-500 h-56 w-full" />
-                <div className="bg-blue-500 h-56 w-full" />
-                <div className="bg-blue-500 h-56 w-full" />
-                <div className="bg-blue-500 h-56 w-full" />
-                <div className="bg-blue-500 h-56 w-full" />
+                <LearnHeader title="Spanish" />
+                <div className="h-56 mb-4 w-full bg-blue-500" />
+                <div className="h-56 pb-4 w-full bg-blue-500" />
+                <div className="h-56 pb-4 w-full bg-blue-500" />
+                <div className="h-56 pb-4 w-full bg-blue-500" />
+                <div className="h-56 pb-4 w-full bg-blue-500" />
+                <div className="h-56 pb-4 w-full bg-blue-500" />
+                <div className="h-56 pb-4 w-full bg-blue-500" />
             </FeedWrapper>
         </div>
     )
